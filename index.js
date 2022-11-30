7a788fbffd54f37599fde4091faa212f1246b4a6
r0mell <rommel.pumisacho@gmail.com>
2022-11-29 21:59:16 -0500

    [FIX] variables de entorno

--- index.js
+++ index.js
@@ -1,32 +1,26 @@
 const app = require('./app');
 const mongoose = require('mongoose');
-const port = 3001;
+const port = process.env.PORT || 3001;
 require('dotenv').config();
-
 
 const user = 'admin';
 const password = '201410509Unick';
-const bd = 'testDb'
+const bd = 'testDb';
 
 //String de coneccion con variables de entorno
-/* const uri = `mongodb+srv://${process.env.USER}:${process.env.PASSWORD}@unick-tets.rznrs.mongodb.net/${process.env.DATABASE}?retryWrites=true&w=majority`;
- */
+const uri = `mongodb+srv://${process.env.USER}:${process.env.PASSWORDDB}@unick-tets.rznrs.mongodb.net/${process.env.DATABASE}?retryWrites=true&w=majority`;
+
 //String de coneccion con variables locales
-const uri = `mongodb+srv://${user}:${password}@unick-tets.rznrs.mongodb.net/${bd}?retryWrites=true&w=majority`; 
+//const uri = `mongodb+srv://${user}:${password}@unick-tets.rznrs.mongodb.net/${bd}?retryWrites=true&w=majority`;
 
-
-mongoose.connect(uri)
+mongoose
+  .connect(uri)
   .then(() => {
-
     console.log('Base de datos conectada');
     app.listen(port, () => {
       console.warn('mi port es ' + port);
-    })
+    });
   })
   .catch((e) => {
-    console.log(e)
-  })
-
-
-
-
+    console.log(e);
+  });
